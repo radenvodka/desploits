a0ae580e36ca55dd6acc38912227fb4963afdda4
Eka Syahwan <ekasyahwan.dev@gmail.com>
2016-10-04 02:52:08 +0700

    add functionality updates

--- 1.0.0/package/desploits.class.php
+++ 1.0.0/package/desploits.class.php
@@ -50,6 +50,56 @@ class Desploit
 		    } 
 	  	return $_ARG; 
 	}
+	function update(){
+		$desploits 	= new Modules;
+		$version  	= json_decode($desploits->ngecurl("http://ekasyahwan.github.io/version/desploits.json"),true);
+		$ver 		= $version[release][version];
+		if( $this->Config()['ver'] < $ver ){
+			$repofile   = $version[repository][files].$ver."/";
+			$desploits->msg("[Update] updates are available");
+			$desploits->msg("[Update] Last test   : ".$this->Config()['ver']);
+			$desploits->msg("[Update] New version : ".$version[release][version]);
+			$desploits->msg("[Update] + Create directory");
+			foreach ($version[required][dir] as $key => $value) {
+				if( mkdir($value) ){
+					$desploits->msg("[Update] > {success} ".$value." Success");
+				}else{
+					$desploits->msg("[Update] > {failed}  ".$value." Failed");
+				}
+			}
+			$desploits->msg("[Update] + Get & install new files");
+			foreach ($version[required][file] as $key => $value) {
+				$getFile = $desploits->ngecurl($repofile.$value['name']);
+				$getName = $value['name'].".update";
+				if($getFile != ""){
+					if(file_put_contents($getName , $getFile)){
+						$desploits->msg("[Update] >>> {Download} ".$value['name']." | Success");
+						if( file_put_contents($value['name'], file_get_contents($getName))){
+								$desploits->msg("[Update]   ^ {Installs}  ".$value['name']." | Success");
+								unlink($getName);
+							}else{
+								$desploits->msg("[Update] MD5 File corrupt , pleas manual download https://github.com/ekasyahwan/desploits.");
+								exit();
+						}
+					}
+				}else{
+					$desploits->msg("[Update] MD5 File corrupt , pleas manual download https://github.com/ekasyahwan/desploits.");
+					exit();
+				}
+			}
+			if(count($version[required][remove]) != ""){
+			$desploits->msg("[Update] + Remove File");
+				foreach ($version[required][remove] as $key => $value) {
+					if(unlink($value)){
+						$desploits->msg("[Update] > {success} ".$value." Success");
+					}
+				}
+			}
+			$desploits->msg("[Update] Done !!! please report it to us for the problems in these tools in our github page https://github.com/ekasyahwan/desploits");
+		}else{
+			$desploits->msg("[Update] no updates");
+		}
+	}
 }
 $desploit 	= new Desploit;
 $fadmin 	= new Fadmin;
