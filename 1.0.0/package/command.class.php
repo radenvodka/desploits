42913c11c6f4eda6c253aeffd85788ff5dc79bcc
Eka Syahwan <ekasyahwan.dev@gmail.com>
2016-10-04 02:51:56 +0700

    add functionality updates

--- 1.0.0/package/command.class.php
+++ 1.0.0/package/command.class.php
@@ -3,6 +3,9 @@ $base = base64_decode("ZGVzcGxvaXRzLnBocA==");
 if($command[input][0] === $base && $command[input][1] === "--help"){
 	$desploit->help();
 }
+if($command[input][0] === $base && $command[input][1] === "--update"){
+	$desploit->update();
+}
 if($command[input][0] === $base && $command[url] && $command[fadmin]){
 	$fadmin->single($command[url],$command[fadmin]);
 }
