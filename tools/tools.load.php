6a0937ad3a08fe31bbba3c07b09afd7c06b39373
Eka Syahwan <ekasyahwan.dev@gmail.com>
2016-11-13 23:06:21 -0500

    load and report tools

---
+++ tools/tools.load.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @Author: Logika Galau
+ * @Date:   2016-11-10 21:47:21
+ * @Last Modified by:   Logika Galau
+ * @Last Modified time: 2016-11-13 22:44:04
+ */
+require_once("tools/class.adminfinder.php");
+$AdminFinder = new AdminFinder;
